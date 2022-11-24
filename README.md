8fd49865ce783c1f01df92c21714be42e9572920
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2022-11-24 14:32:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drawing-board
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kndukh)
---
+++ script.js
@@ -0,0 +1,37 @@
+const board = document.querySelector("#board");
+const colors = ['rgb(255, 127, 249)', 'rgb(84, 98, 221)', 'rgb(100, 248, 248)', 'rgb(101, 255, 121)', 'rgb(252, 255, 101)', 'rgb(255, 170, 101)', 'rgb(255, 118, 118)', 'rgb(219, 129, 255)']
+const squaresNumbers = 500;
+
+for (let i = 0; i < squaresNumbers; i++) {
+ const square = document.createElement('div');
+ square.classList.add('square');
+
+
+ square.addEventListener('mouseover', () => setColor(square));
+ square.addEventListener('touchstart', () => setColor(square));
+ square.addEventListener('mouseleave', () => removeColor(square));
+ square.addEventListener('touchend', () => removeColor(square));
+
+ board.append(square);
+
+
+
+}
+
+function setColor(element) {
+const color = getRandomColor();
+element.style.backgroundColor = color;
+element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`; 
+}
+
+
+function removeColor(element) {
+element.style.backgroundColor = 'rgb(90, 90, 90)';
+element.style.boxShadow = `0 0 2px black`
+}
+
+function getRandomColor() {
+const index = Math.floor(Math.random() * colors.length);
+return colors[index];
+}
+
